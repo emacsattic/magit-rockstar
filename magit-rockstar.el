3eb7353f110dcda48dcbbeb88590f4912272f0e8
Jonas Bernoulli <jonas@bernoul.li>
2015-07-09 15:35:38 +0200

    add new command magit-branch-pull-request

--- magit-rockstar.el
+++ magit-rockstar.el
@@ -112,6 +112,25 @@ export GIT_COMMITTER_DATE=\"%%s %s\";;" tz tz)))
   (--each (magit-section-children section)
     (magit-debug-sections-1 it (1+ level))))
 
+(defun magit-branch-pull-request (number &optional branch checkout)
+  "Create a new branch from a Github pull request and show its log.
+Read \"NR[:BRANCH-NAME] from the user.  If BRANCH-NAME is not provided
+use \"pr-NR\".  Assume all pull requests can be found on \"origin\".
+With a prefix argument checkout branch instead of showing its log."
+  (interactive
+   (let ((input (magit-read-string "Branch pull request (NR[:BRANCH-NAME])")))
+     (if (string-match "\\([1-9][0-9]*\\)\\(?::\\(.+\\)\\)?" input)
+         (list (match-string 1 input)
+               (match-string 2 input)
+               current-prefix-arg)
+       (user-error "Invalid input"))))
+  (unless branch
+    (setq branch (format "pr-%s" number)))
+  (magit-call-git "fetch" "origin" (format "pull/%s/head:%s" number branch))
+  (if checkout
+      (magit-run-git "checkout" branch)
+    (magit-log (list branch))))
+
 ;;; magit-rockstar.el ends soon
 (provide 'magit-rockstar)
 ;; Local Variables:
